0c20b0f170c20e1dbae3a91cd280012a32f3f740
Keisuke Shimokawa <shimoke4869@gmail.com>
2024-06-15 21:37:07 +0900

    Block構文を追加

---
+++ examples/03_group.rs
@@ -0,0 +1,113 @@
+#[derive(Debug, PartialEq, Eq)]
+enum Value<'src> {
+    Num(i32),
+    Op(&'src str),
+    Block(Vec<Value<'src>>),
+}
+
+impl<'src> Value<'src> {
+    fn as_num(&self) -> i32 {
+        match self {
+            Self::Num(val) => *val,
+            _ => panic!("Value is not a number"),
+        }
+    }
+}
+
+fn main() {
+    for line in std::io::stdin().lines().flatten() {
+        parse(&line);
+    }
+}
+
+fn parse(line: &str) -> Vec<Value> {
+    let mut stack = vec![];
+    let input: Vec<_> = line.split(' ').collect();
+    let mut words = &input[..];
+
+    // 最後まで読み込むまで繰り返す
+    while let Some((&word, mut rest)) = words.split_first() {
+        if word.is_empty() {
+            break;
+        } else if word == "{" {
+            let value;
+            (value, rest) = parse_block(rest);
+            stack.push(value);
+        } else if let Ok(parsed) = word.parse::<i32>() {
+            stack.push(Value::Num(parsed));
+        } else {
+            match word {
+                "+" => add(&mut stack),
+                "-" => sub(&mut stack),
+                "*" => mul(&mut stack),
+                "/" => div(&mut stack),
+                _ => stack.push(Value::Op(word)),
+            }
+        }
+        words = rest;
+    }
+    println!("{:?}", stack);
+
+    stack
+}
+
+fn parse_block<'src, 'a>(input: &'a [&'src str]) -> (Value<'src>, &'a [&'src str]) {
+    let mut tokens = vec![];
+    let mut words = input;
+
+    while let Some((&word, mut rest)) = words.split_first() {
+        if word.is_empty() {
+            break;
+        } else if word == "{" {
+            let value;
+            (value, rest) = parse_block(rest);
+            tokens.push(value);
+        } else if word == "}" {
+            return (Value::Block(tokens), rest);
+        } else if let Ok(value) = word.parse::<i32>() {
+            tokens.push(Value::Num(value));
+        } else {
+            tokens.push(Value::Op(word));
+        }
+        words = rest;
+    }
+
+    (Value::Block(tokens), words)
+}
+
+fn add(stack: &mut Vec<Value>) {
+    let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(lhs + rhs));
+}
+
+fn sub(stack: &mut Vec<Value>) {
+    let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(lhs - rhs));
+}
+
+fn mul(stack: &mut Vec<Value>) {
+    let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(lhs * rhs));
+}
+
+fn div(stack: &mut Vec<Value>) {
+    let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(lhs / rhs));
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{parse, Value::*};
+
+    #[test]
+    fn test_group() {
+        assert_eq!(
+            parse("1 2 + { 3 4 }"),
+            vec![Num(3), Block(vec![Num(3), Num(4)])]
+        )
+    }
+}
